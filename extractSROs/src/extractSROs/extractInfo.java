2e5362b532659600e43ae7e5cc5138e51868532c
gelopez18 <94268643+gelopez18@users.noreply.github.com>
2022-10-05 12:03:24 -0600

    system can received files name to open them

--- extractSROs/src/extractSROs/extractInfo.java
+++ extractSROs/src/extractSROs/extractInfo.java
@@ -1,13 +1,32 @@
-	package extractSROs;
-	import java.io.File;
-	import java.io.IOException;
+package extractSROs;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class extractInfo {
 		public static void main(String[] arg) throws Exception{
-			String pathWay = "\\Users\\gab54\\OneDrive\\Documents\\JavaPractice\\";
-			Path1 pth = new Path1(pathWay); //we instantiate a new object created in path under this same package and passed on the path
-			pth.openFile("JavaFileTest.xls"); /*then the method needs to receive the name of the file
-			so it can be added to the path and the IO package can open it*/
+					String pathWay = "\\Users\\gab54\\OneDrive\\Documents\\JavaPractice\\";
+			Scanner Uinput = new Scanner(System.in);
+				System.out.println("Enter File's name:");
+					String File = Uinput.next();
+
+					pathWay += File;
+
+			File file = new File(pathWay);
+					if(!Desktop.isDesktopSupported()){
+			            System.out.println("Desktop is not supported");
+			        }
+			Desktop desktop = Desktop.getDesktop();
+		      
+				 if (file.isFile() && file.exists()) {
+					 desktop.open(file);
+			            System.out.println("File open");
+			        }
+			        else {
+			            System.out.println("File either not exist"
+			                               + " or can't open");
+			        }
 		}
 	}
 
