12a2255f17973b0ec9f818c62c70aa909520e402
gelopez18 <94268643+gelopez18@users.noreply.github.com>
2022-10-06 11:37:35 -0600

    Update extractInfo.java

--- extractSROs/src/extractSROs/extractInfo.java
+++ extractSROs/src/extractSROs/extractInfo.java
@@ -11,16 +11,28 @@ public class extractInfo {
 					String FileName = Uinput.next();
 		// Create a file input stream on specified path with the created file
 					 File file =  ModifyFile.Open(FileName);
-			FileInputStream fis = new FileInputStream(file); 
+		//Step 1: Attach a file to a FileInputStream as this will enable us to read data from the file as shown below as follows:		 
+			FileInputStream fis = new FileInputStream(file);
+			// Illustrating getChannel() method
 				System.out.println(fis.getChannel());
+				  // Illustrating getFD() method
 				System.out.println(fis.getFD());
+				// Illustrating available method
 				System.out.println("Number of remaining bytes:"
                         + fis.available());
-				
+				// Illustrating skip() method
 				fis.skip(4);
 				System.out.println("FileContents :");
+				 
+		        // Reading characters from FileInputStream
+		        // and write them
 				int ch;
-				while ((ch = fis.read()) != -1)
+				/*fis.read() Step 2: Now in order to read data from the file, 
+				 * we should read data from the FileInputStream as shown below:
+				 */
+				
+				// Holds true till there is data inside file
+				while ((ch = fis.read()) != -1) //Step 3-A: When there is no more data available to read further, the read() method returns -1; 
 		            System.out.print((char)ch);
 				 fis.close();
 				 ModifyFile.Close(FileName);
